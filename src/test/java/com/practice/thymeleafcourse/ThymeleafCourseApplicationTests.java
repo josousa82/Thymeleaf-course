448d263d53b9ff8bee782d833ac14b7e81852e51
sousajo <josesousa@rentalcars.com>
2020-09-22 20:04:08 +0100

    initial coomit

---
+++ src/test/java/com/practice/thymeleafcourse/ThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.thymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
