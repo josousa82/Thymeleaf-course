ba4d1aa9722e4e286bdffa0ce200ba0b9f1e68c5
sousajo <josesousa@rentalcars.com>
2020-09-23 23:15:58 +0100

    configured internationalization on spring and thymeleaf. Closes #12

---
+++ src/main/java/com/practice/thymeleafcourse/config/SpringMvcConfiguration.java
@@ -0,0 +1,41 @@
+package com.practice.thymeleafcourse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * Created by sousaJ on 23/09/2020
+ * in package - com.practice.thymeleafcourse.config
+ **/
+@Configuration
+public class SpringMvcConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+     public LocaleChangeInterceptor localeChangeInterceptor() {
+
+         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+         localeChangeInterceptor.setParamName("lang");
+
+         return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
