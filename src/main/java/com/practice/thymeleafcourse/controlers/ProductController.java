8b23ae86543ec08a4904bf9ef13101429429bdd1
sousajo <josesousa@rentalcars.com>
2020-09-24 14:53:27 +0100

    Configured Product to get dynamic content from controller product.html. Closes #18

--- src/main/java/com/practice/thymeleafcourse/controlers/ProductController.java
+++ src/main/java/com/practice/thymeleafcourse/controlers/ProductController.java
@@ -3,6 +3,7 @@ package com.practice.thymeleafcourse.controlers;
 import com.practice.thymeleafcourse.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,7 +27,7 @@ public class ProductController {
 
 
     @RequestMapping("/product/{id}")
-    public String getProductById(@RequestParam Integer id, Model model){
+    public String getProductById(@PathVariable Integer id, Model model){
         model.addAttribute("product", productService.getProductById(id));
         return "product";
     }
