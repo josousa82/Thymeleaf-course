ee89e89cfb1066c91463bf25b756fbe8a285d8fe
sousajo <josesousa@rentalcars.com>
2020-09-24 13:39:01 +0100

    Created created service layer for Product. Closes #16

---
+++ src/main/java/com/practice/thymeleafcourse/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.practice.thymeleafcourse.services;
+
+import com.practice.thymeleafcourse.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by sousaJ on 24/09/2020
+ * in package - com.practice.thymeleafcourse.services
+ **/
+public interface ProductService {
+    Product getProduct(Integer id);
+    List<Product> listProducts();
+}
